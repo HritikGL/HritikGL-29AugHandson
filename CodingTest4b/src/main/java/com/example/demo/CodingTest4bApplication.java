af791a7b2bd6735becb3ef6d1838c7b0bd7155f2
HritikGL <110605084+HritikGL@users.noreply.github.com>
2022-08-29 11:25:18 +0530

    Question2 (SpringBoot Testing)

---
+++ CodingTest4b/src/main/java/com/example/demo/CodingTest4bApplication.java
@@ -0,0 +1,14 @@
+/*2)write a spring (boot) application and test those application*/
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTest4bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingTest4bApplication.class, args);
+	}
+
+}
---
+++ CodingTest4b/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,47 @@
+package com.example.demo.model;
+
+public class Student {
+	
+	 private String id;
+
+	 private String name;
+
+	 private String standard;
+	 
+	 private double fees;
+
+	 public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStandard() {
+		return standard;
+	}
+
+	public void setStandard(String standard) {
+		this.standard = standard;
+	}
+
+	public double getFees() {
+		return fees;
+	}
+
+	public void setFees(double fees) {
+		this.fees = fees;
+	}
+
+
+
+}
